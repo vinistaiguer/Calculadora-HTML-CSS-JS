a769122ad1547a92d4ffbcc70cd4a04c2ad1d59b
Vinicius Staiguer <viniciusstaiguer@hotmail.com>
2021-05-03 13:34:29 -0300

    js

--- js/script.js
+++ js/script.js
@@ -5,20 +5,19 @@ function insert(num){
  
 
  function clean(){
-    document.getElementById('resultado').innerHTML ="";
-}
-
-function back(){
-    let resultado = document.getElementById('resultado').innerHTML;
-    document.getElementById('resultado').innerHTML = resultado.substring(0, resultado.length -1);
-}
-
-function calcular(){
-    
-    var resultado = document.getElementById('resultado').innerHTML;
-        if (resultado){
-            document.getElementById('resultado').innerHTML = eval(resultado);
-        }else{
-            document.getElementById('resultado').innerHTML = "Sem Valores";
-        }
-}
+     document.getElementById('resultado').innerHTML ="";
+ }
+ 
+ function back(){
+     let resultado = document.getElementById('resultado').innerHTML;
+     document.getElementById('resultado').innerHTML = resultado.substring(0, resultado.length -1);
+ }
+ 
+ function calcular(){
+     var resultado = document.getElementById('resultado').innerHTML;
+         if (resultado){
+             document.getElementById('resultado').innerHTML = eval(resultado);
+         }else{
+             document.getElementById('resultado').innerHTML = "Sem Valores";
+         }
+ }
