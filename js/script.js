f7d7686e291b356828bab0fa76e6c2a517c296a9
Vinicius Staiguer <viniciusstaiguer@hotmail.com>
2021-05-04 13:05:10 -0300

    alteração js

--- js/script.js
+++ js/script.js
@@ -13,10 +13,52 @@ function back(){
 }
 
 function calcular(){
+    let calcular = document.getElementById('resultado').innerHTML;
+    document.getElementById('resultado').innerHTML = resultado.substring(resultado.length);
+}
+
+
+function calcular(){
+    function somar(number, num){
+        return number + num;
+    }
+
+    function subtrair(number, num){
+        return number - num;
+    }
+
+    function multiplicar(number, num){
+        return number * num;
+    }
+
+    function dividir(number, num){
+        if(num == 0){
+            return "Erro"
+        }else{
+        return number / num;
+        }
+    }
+}
+
+
+
+/*
+document.addEventListener('keydown', handleKeydow)
+
+function handleKeydow(event){
+    const keyPressed = event.key
+    if(keyPressed === Number){
+        console.log("ok ok")
+    }else{
+        console.log("ERROU!")
+    }
+}
+
+function calcular(){
     var resultado = document.getElementById('resultado').innerHTML;
         if (resultado){
             document.getElementById('resultado').innerHTML = eval(resultado);
         }else{
             document.getElementById('resultado').innerHTML = "Sem Valores";
         }
-}
+}*/
