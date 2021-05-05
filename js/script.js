bb6ade8976d16088739f94ea353b996ad49989c3
Vinicius Staiguer <viniciusstaiguer@hotmail.com>
2021-05-05 00:52:42 -0300

    alteração do js para retirar eval

--- js/script.js
+++ js/script.js
@@ -1,64 +1,64 @@
+let operador = "";
+
 function insert(num){
+    if(num === "+" || num === "-" || num === "*" || num === "/" ){
+        operador = num;  
+    }
+
     let number = document.getElementById('resultado').innerHTML;
    document.getElementById('resultado').innerHTML = number + num;
 }
 
+
 function clean(){
     document.getElementById('resultado').innerHTML ="";
 }
+
 
 function back(){
     let resultado = document.getElementById('resultado').innerHTML;
     document.getElementById('resultado').innerHTML = resultado.substring(0, resultado.length -1);
 }
 
-function calcular(){
-    let calcular = document.getElementById('resultado').innerHTML;
-    document.getElementById('resultado').innerHTML = resultado.substring(resultado.length);
+
+function somar(number, num){
+    return parseInt(number) + parseInt(num) ;
 }
 
 
-function calcular(){
-    function somar(number, num){
-        return number + num;
-    }
-
-    function subtrair(number, num){
-        return number - num;
-    }
-
-    function multiplicar(number, num){
-        return number * num;
-    }
-
-    function dividir(number, num){
-        if(num == 0){
-            return "Erro"
-        }else{
-        return number / num;
-        }
-    }
+function subtrair(number, num){
+    return number - num;
 }
 
 
+function multiplicar(number, num){
+    return number * num;
+}
 
-/*
-document.addEventListener('keydown', handleKeydow)
 
-function handleKeydow(event){
-    const keyPressed = event.key
-    if(keyPressed === Number){
-        console.log("ok ok")
+function dividir(number, num){
+    if(num == 0){
+        return "Erro"
     }else{
-        console.log("ERROU!")
+    return number / num;
     }
 }
 
+
 function calcular(){
-    var resultado = document.getElementById('resultado').innerHTML;
-        if (resultado){
-            document.getElementById('resultado').innerHTML = eval(resultado);
-        }else{
-            document.getElementById('resultado').innerHTML = "Sem Valores";
-        }
-}*/
+    let resultado = document.getElementById('resultado').innerHTML;
+    let texto = resultado.split(operador);
+    if(operador === "+"){
+        const calculo = somar(texto[0], texto[1]);
+        return clean(), insert(calculo);
+    }else if(operador === "-"){
+        const calculo = subtrair(texto[0], texto[1]);
+        return clean(), insert(calculo);
+    }else if(operador === "*"){
+        const calculo = multiplicar(texto[0], texto[1]);
+        return clean(), insert(calculo);
+    }else if(operador === "/"){
+        const calculo = dividir(texto[0], texto[1]);
+        return clean(), insert(calculo);
+    }
+}
