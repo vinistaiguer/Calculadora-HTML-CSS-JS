648e34e7fc997135bcd9ea67e4cff679bc171be4
Vinicius Staiguer <viniciusstaiguer@hotmail.com>
2021-05-09 17:09:45 -0300

    arrow function javascript

--- js/script.js
+++ js/script.js
@@ -1,56 +1,55 @@
 let operador = "";
 
-function insert(num){
-    if(num === "+" || num === "-" || num === "*" || num === "/" ){
-        operador = num;  
+function insert(num) {
+    if (num === "+" || num === "-" || num === "*" || num === "/") {
+        operador = num;
     }
 
     let number = document.getElementById('resultado').innerHTML;
-   document.getElementById('resultado').innerHTML = number + num;
+    document.getElementById('resultado').innerHTML = number + num;
 }
 
 
-function clean(){
-    document.getElementById('resultado').innerHTML ="";
+function clean() {
+    document.getElementById('resultado').innerHTML = "";
 }
 
 
-function back(){
+function back() {
     let resultado = document.getElementById('resultado').innerHTML;
-    document.getElementById('resultado').innerHTML = resultado.substring(0, resultado.length -1);
+    document.getElementById('resultado').innerHTML = resultado.substring(0, resultado.length - 1);
 }
 
 
 const somar = (number, num) => parseInt(number) + parseInt(num);
 
-const subtrair = ( number, num ) => number - num; 
+const subtrair = (number, num) => number - num;
 
-const multiplicar = ( number, num) =>  number * num; 
+const multiplicar = (number, num) => number * num;
 
-function dividir(number, num){
-    if(num == 0){
+function dividir(number, num) {
+    if (num == 0) {
         return "Erro"
-    }else{
-    return number / num;
+    } else {
+        return number / num;
     }
 }
 
 
-function calcular(){
+function calcular() {
     let resultado = document.getElementById('resultado').innerHTML;
     let texto = resultado.split(operador);
-    if(operador === "+"){
+    if (operador === "+") {
         const calculo = somar(texto[0], texto[1]);
         return clean(), insert(calculo);
-    }else if(operador === "-"){
+    } else if (operador === "-") {
         const calculo = subtrair(texto[0], texto[1]);
         return clean(), insert(calculo);
-    }else if(operador === "*"){
+    } else if (operador === "*") {
         const calculo = multiplicar(texto[0], texto[1]);
         return clean(), insert(calculo);
-    }else if(operador === "/"){
+    } else if (operador === "/") {
         const calculo = dividir(texto[0], texto[1]);
         return clean(), insert(calculo);
     }
 }
-
